ba6541876fc799fbace6ccfdc36b6d63d78d6cdf
Andrew Silva <andrewsilva9@gmail.com>
2019-05-09 00:49:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,14 +9,13 @@ class Dog
     @@all << self
   end
 
-  def self.clear_all
-    @@all.clear
-
-  end
-
   def self.all
     @@all.each do |pet|
       puts pet.name
     end
+  end
+
+  def self.clear_all
+    @@all.clear
   end
 end
